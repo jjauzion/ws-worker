bc5df6ad65f58dab0199b174a9f56a77a9306f1e
jjauzion <jeremy.jauzion@gmail.com>
2021-03-10 20:11:22 +0100

    added iota to define error code

--- client/grpc_error.go
+++ client/grpc_error.go
@@ -5,12 +5,16 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const (
+	CodeNoTaskinQueue = iota + 600
+)
+
 var (
 	pebkac = func(s string, errCode codes.Code) error { return status.Error(errCode, s) }
 
 	errBadReq         = pebkac("bad request data", codes.InvalidArgument)
 	errForbidden      = pebkac("forbidden", codes.PermissionDenied)
-	errNoTasksInQueue = pebkac("no tasks in queue", codes.Code(600))
+	errNoTasksInQueue = pebkac("no tasks in queue", CodeNoTaskinQueue)
 )
 
 func exactlyOneOf(fields string) error {
