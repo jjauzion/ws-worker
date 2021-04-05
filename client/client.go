ddfb9d34291090c6cfc0f0446fac95b4a7dd6ecc
jjauzion <jeremy.jauzion@gmail.com>
2021-04-05 18:34:33 +0200

    sleep between call put in config file

--- client/client.go
+++ client/client.go
@@ -4,20 +4,15 @@ import (
 	"context"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"log"
 	"time"
 
 	pb "github.com/jjauzion/ws-worker/proto"
 )
 
-const (
-	sleepBetweenCall = 30 * time.Second
-)
-
 func Run() {
 	lg, cf, dh, err := dependencies()
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
 	address := cf.WS_GRPC_HOST + ":" + cf.WS_GRPC_PORT
 	lg.Info("connecting to grpc server", zap.String("address", address))
@@ -30,27 +25,29 @@ func Run() {
 	c := pb.NewApiClient(conn)
 
 	ctx := context.Background()
-	lg.Info("pulling new task...", zap.Duration("sleep", sleepBetweenCall))
+	lg.Info("pulling new task...", zap.Duration("sleep", cf.WS_SLEEP_BETWEEN_CALL))
 	for {
 		r, err := c.StartTask(ctx, &pb.StartTaskReq{WithGPU: true})
 		if getErrorCode(err) == getErrorCode(errNoTasksInQueue) {
-			time.Sleep(sleepBetweenCall)
+			time.Sleep(cf.WS_SLEEP_BETWEEN_CALL)
 			continue
 		} else if err != nil {
 			lg.Error("failed to start task", zap.Error(err))
-			time.Sleep(sleepBetweenCall)
+			time.Sleep(cf.WS_SLEEP_BETWEEN_CALL)
 			continue
 		}
 		lg.Info("starting task", zap.String("id", r.TaskId))
 		containerLogs, err := dh.runImage(ctx, r.Job.DockerImage, r.Job.Env)
 		var errString string
 		if err != nil {
+			lg.Error("container run failed", zap.Error(err))
 			errString = err.Error()
 		}
 		_, err = c.EndTask(ctx, &pb.EndTaskReq{TaskId: r.TaskId, Error: errString, Logs: containerLogs})
 		if err != nil {
 			lg.Error("failed to end task", zap.Error(err))
+		} else {
+			lg.Info("task ended", zap.String("id", r.TaskId))
 		}
-		lg.Info("task ended", zap.String("id", r.TaskId))
 	}
 }
